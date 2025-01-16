4ded344136313bac9722cfd0cfc109c9ad122486
Ryan Constable <RyanMConstable@gmail.com>
2025-01-16 22:23:23 +0000

    Many edits most important was fixing environment variables through cron

--- minecraft_bot.py
+++ minecraft_bot.py
@@ -3,6 +3,10 @@
 import discord
 import os, subprocess # default module
 from discord.ext import commands
+from dotenv import load_dotenv
+
+
+load_dotenv("/home/president/minecraft/.env")
 
 intents = discord.Intents.default()
 intents.message_content = True
--- minecraft_status.py
+++ minecraft_status.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 
+from dotenv import load_dotenv
 from mcstatus import JavaServer
 import subprocess
 import os
 
-server = JavaServer(os.getenv("MYIP"), 25565)
+if __name__ == "__main__":
+    load_dotenv('/home/president/minecraft/.env')
 
-status = server.status()
-result = subprocess.run(["ps", "ax", "|", "grep", "minecraft_server"], capture_output=True, text=True)
+    try:
+        server = JavaServer(os.getenv("MYIP"), 25565)
+        status = server.status()
+    except Exception as e:
+        print("Server is down or ip is wrong, most likely server is down")
+        exit()
 
-print(result)
-print()
-print(f"{status}")
-print()
-print(status.players.online)
+    players_online = status.players.online
+
+    if players_online == 0:
+        os.system("echo 0 >> /home/president/minecraft/test")
+        f = open("/home/president/minecraft/test")
+        minutes_with_no_players = len(f.readlines())
+        f.close()
+        if minutes_with_no_players >= 5:
+            os.system("rm /home/president/minecraft/test")
+            os.system("ps ax | grep minecraft_server.jar | grep -v grep | awk '{print $1}' > /home/president/minecraft/mcserverps")
+            f = open("/home/president/minecraft/mcserverps")
+            mcserverps = f.read().splitlines()[0]
+            f.close()
+            os.system("rm /home/president/minecraft/mcserverps")
+            print(mcserverps)
+            os.system(f"kill {mcserverps}")
+    else:
+        if os.path.exists("/home/president/minecraft/test"):
+            os.system("rm /home/president/minecraft/test")
